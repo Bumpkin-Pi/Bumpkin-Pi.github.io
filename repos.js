a50267ed01f9bb58f21b7514fded729f0ccc4ae8
Holly <h.person.notrobot@gmail.com>
2023-01-26 11:09:03 +0000

    Fixed null on no description

---
+++ repos.js
@@ -0,0 +1,39 @@
+const url = "https://api.github.com/users/Bumpkin-Pi/repos"
+async function main(){
+	let info;
+	let shown_repos = []
+	await fetch(url)
+	   .then( r => r.text() )
+	   .then( t => {
+	   	info = JSON.parse(t)
+	   })
+	// console.log(info)
+	for (repo in info){
+		if (!info[repo].archived && info[repo].name != "Bumpkin-Pi" && info[repo].name != "Bumpkin-Pi.github.io"){
+			// console.log(info[repo])
+			let html = await fetch(info[repo].html_url)
+			   .then( r => r.text() )
+			   .then( t => {
+			   	return t;
+			})
+			let image = html.slice(html.lastIndexOf("<meta property=\"og:image\" content=\"")).split("\" />")[0].split("content=\"")[1]
+			// console.log(image)
+			// <meta property="og:image" content="https://avatars.githubusercontent.com/u/74017165?s=400&amp;v=4">
+			console.log(info[repo])
+			let repo_div = document.createElement("div")
+			repo_div.innerHTML = `
+			<a href="${info[repo].html_url}">
+							<div class="repo">
+							  <div class="image-container">
+							    <img src="${image}">
+							  </div>
+							  <h1>${info[repo].name}</h1>
+							  <h2>${String(info[repo].description).replace("null", "")}</h2>
+							</div></a>
+							`
+			document.body.appendChild(repo_div)
+		}
+	}
+}
+
+main()
